bc2bcb22f4ff949a82de75a4f3c6bd6e9e65df67
ZipCPU <dgisselq@ieee.org>
2023-05-03 16:13:52 -0400

    Updated copyrights to 2023

--- rtl/spicmd.v
+++ rtl/spicmd.v
@@ -11,10 +11,10 @@
 //
 ////////////////////////////////////////////////////////////////////////////////
 // }}}
-// Copyright (C) 2019-2022, Gisselquist Technology, LLC
+// Copyright (C) 2019-2023, Gisselquist Technology, LLC
 // {{{
 // This program is free software (firmware): you can redistribute it and/or
-// modify it under the terms of  the GNU General Public License as published
+// modify it under the terms of the GNU General Public License as published
 // by the Free Software Foundation, either version 3 of the License, or (at
 // your option) any later version.
 //
